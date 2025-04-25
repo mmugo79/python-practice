4b46f1f0a44e92fd87139a3509e7302ba18f8216
mmugo79 <mugomoses506@gmail.com>
2025-04-25 13:42:52 +0300

    commit 5

--- task 4.py
+++ task 4.py
@@ -6,3 +6,10 @@ index= length -1
 while index>=0:
     reversed_text += text[index]
     index -=1
+
+    return reversed_text
+
+#example usage
+original = "zytho$n"
+reversed_result = reverse_string(original)
+

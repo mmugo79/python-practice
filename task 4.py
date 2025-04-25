69eb97e20513fbf190a774c84f8b429c42a1a347
mmugo79 <mugomoses506@gmail.com>
2025-04-25 14:34:11 +0300

    commit 10

--- task 4.py
+++ task 4.py
@@ -1,16 +1,16 @@
 def reverse_string(text):
-    reversed_text=""
+    reversed_text="" #start with an empty string
     length = len(text)
+    index= length -1
+    #loop from last character to first
+    while index>=0:
+      reversed_text += text[index]#add each character to the result
+      index -=1#move backwards
 
-index= length -1
-while index>=0:
-    reversed_text += text[index]
-    index -=1
-
-    return reversed_text
+    return reversed_text #return results
 
 #example usage
-original = "zytho$n"
+original = "MOSES"
 reversed_result = reverse_string(original)
 print("reversed_string:", reversed_result)
 

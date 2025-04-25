f8ac1cb3e33515a5ba2772ab1e38e354b9426b85
mmugo79 <mugomoses506@gmail.com>
2025-04-25 14:26:12 +0300

    commit 10

--- task 1.py
+++ task 1.py
@@ -1,11 +1,11 @@
 def sum_list(numbers):
-    total = 0
-    for x in numbers:
-        total = total + x
+    total = 0 #initialize total to 0
+    for x in numbers:#loop elements in the list
+        total = total + x#add each element to the total
     return total
 
 #create a list
 my_list = [10, 20, 30, 40, 50]
-
+#call the function to output the results
 print(sum_list(my_list))#output should be 150
 

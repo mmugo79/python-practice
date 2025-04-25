5baf20202be8e72e8da0b2daf675ec4f06ce5cf7
mmugo79 <mugomoses506@gmail.com>
2025-04-25 14:16:40 +0300

    commit

--- task 3.py
+++ task 3.py
@@ -1,12 +1,10 @@
-def factroial(n):
+def factorial(n):
     result = 1
-
-for i in range (1, n+1):
-    result *= i
+    for i in range (1, n + 1):
+        result *= i
     return result
 
 #example usage
-number=5
-
-results = factorial(number)
-print(f"the factorial of{number} is{result}")
+number = 5
+result = factorial(number)
+print(f"the factorial of{number} is {result}")

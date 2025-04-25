0fe9299ee5627edfdcc771c7785b2604a300679b
mmugo79 <mugomoses506@gmail.com>
2025-04-25 14:36:29 +0300

    commit final

--- task 5.py
+++ task 5.py
@@ -1,7 +1,10 @@
+#function to calculate factorial using recursion
 def factorial(n):
+    # if n is 0, return 1
     if n ==0:
         return 1
     else:
+        #recurisve case
         return n * factorial(n-1)
 
 #example usage

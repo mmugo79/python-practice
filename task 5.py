d97ee411db002a70bc5e4f6ea456fd49ffac9c15
mmugo79 <mugomoses506@gmail.com>
2025-04-25 13:52:07 +0300

    commit 4

--- task 5.py
+++ task 5.py
@@ -3,3 +3,10 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+    #example usage
+    number = 5
+    result = factorial(number)
+    print(f"the factorial of {number} is {result}")
+    return None
+

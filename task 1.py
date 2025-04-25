5902be1499b2a162b0d3c05826d5d3891d150583
mmugo79 <mugomoses506@gmail.com>
2025-04-25 14:02:30 +0300

    commit 8

--- task 1.py
+++ task 1.py
@@ -1,10 +1,11 @@
 def sum_list(numbers):
     total=0
     for x in numbers:
-        total=total+x
+        total= total + x
         return total
 
     #create a list
     my_list = [10,20,30,40,50]
 
-    print(sum_list)
+    print(sum_list(my_list))
+    return None

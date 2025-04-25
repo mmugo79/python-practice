3e1df69698c9bf1283d35197b0a6cc55264ec834
mmugo79 <mugomoses506@gmail.com>
2025-04-25 14:39:56 +0300

    commit final

--- task 6.py
+++ task 6.py
@@ -1,7 +1,9 @@
+#calculate sum of digits using recursion
 def addDigits(num):
+    #if number is 0,return o
     if num == 0:
         return 0
-
+#recursive case:add the last digit to the results of addDigits on the remaining digits
     return (num % 10) + addDigits(num // 10)
 #example usage
 print(addDigits(1234))#output should be 10

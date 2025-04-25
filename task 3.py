ebb5de24cf529b077f87d147f3c509d00cf4a11d
mmugo79 <mugomoses506@gmail.com>
2025-04-25 14:31:30 +0300

    commit 10

--- task 3.py
+++ task 3.py
@@ -1,10 +1,11 @@
+#a function to compute factorial using a loop
 def factorial(n):
-    result = 1
-    for i in range (1, n + 1):
-        result *= i
-    return result
+    result = 1 #initalize result to 1
+    for i in range (1, n + 1):#loop from 1 to n
+        result *= i# multiply result by 1 in each step
+    return result#return computed factorial
 
 #example usage
-number = 5
-result = factorial(number)
-print(f"the factorial of{number} is {result}")
+number = 5 #set a random number
+result = factorial(number) #call the factorial function
+print(f"the factorial of{number} is {result}") #output the results
